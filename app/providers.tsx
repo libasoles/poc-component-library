87dd96857cf35bfd662211b48ac299bb840f4105
Guillermo Perez <gperez78@gmail.com>
2024-06-09 20:55:06 -0300

    React query dev tools

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { PropsWithChildren } from "react";
 
 function makeQueryClient() {
@@ -39,6 +40,8 @@ export default function Providers({ children }: PropsWithChildren) {
   const queryClient = getQueryClient();
 
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      {children} <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
