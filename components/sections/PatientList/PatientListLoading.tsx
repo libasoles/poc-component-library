93803bf0c0557df5ed0ccf1a106826e470d4fbf2
Guillermo Perez <gperez78@gmail.com>
2024-06-09 18:01:11 -0300

    Loading skeleton updates

--- components/sections/PatientList/PatientListLoading.tsx
+++ components/sections/PatientList/PatientListLoading.tsx
@@ -1,15 +1,18 @@
+import Column from "@/components/generic/layout/Column";
+import Row from "@/components/generic/layout/Row";
+
 const PatientCardSkeleton = () => {
   return (
-    <li className="mt-6 p-4 bg-zinc-800 rounded-lg shadow-lg">
+    <li className="mt-6 p-4 bg-zinc-800 rounded-lg shadow-lg list-none">
       <div className="animate-pulse h-34">
-        <div className=" flex space-x-4">
+        <Row className="space-x-4">
           <div className="rounded-full bg-slate-700 h-12 w-12"></div>
 
-          <div className="flex flex-col flex-1 space-y-4  justify-center">
+          <Column className="flex-1 space-y-4 justify-center">
             <div className="h-2 bg-slate-700 rounded w-40"></div>
             <div className="h-2 bg-slate-700 rounded w-60"></div>
-          </div>
-        </div>
+          </Column>
+        </Row>
 
         <div className="space-y-3 mt-6">
           <div className="h-2 bg-slate-700 rounded"></div>
@@ -19,7 +22,10 @@ const PatientCardSkeleton = () => {
           </div>
         </div>
 
-        <div className="h-2 bg-slate-700 rounded w-20 mt-6"></div>
+        <Row className="space-x-6 justify-end">
+          <div className="h-2 bg-slate-700 rounded w-10 mt-6 self-end"></div>
+          <div className="h-2 bg-slate-700 rounded w-16 mt-6 self-end"></div>
+        </Row>
       </div>
     </li>
   );
